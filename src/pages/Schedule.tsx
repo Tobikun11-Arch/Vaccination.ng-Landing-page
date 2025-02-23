ffdaade1e2288c47da74da78bce3cbd9982f2064
Joenel <joenelsevellejo831@gmail.com>
2025-02-23 10:21:17 +0800

    [Mobile] Schedule page

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,9 +1,16 @@
 export default function Schedule() {
     return (
-        <main className="bg-white text-black p-10 w-full">
-            <section className="flex justify-between">
+        <main className="bg-white text-black p-4 sm:p-10 w-full">
+            <section className="flex flex-col lg:flex-row justify-between">
                 <div className="w-full">
                     <h1 className="font-bold text-3xl">Get your vaccine <br />registration today</h1>
+                    <div className="w-full lg:hidden justify-center items-center mt-5">
+                        <div className="relative flex justify-center items-center w-full sm:w-2/5">
+                            <img src="/injection.jpg" alt="Injection vaccine" className="w-full h-80 rounded-md"/>
+                            <img src="/coronavirus.png" alt="coronavirus" className="absolute -top-6 -left-9 w-20 h-16"/>
+                            <button className="absolute bottom-5 bg-blue-700/50 shadow-lg backdrop-blur-sm py-1 px-4 rounded-lg font-semibold text-white">Verify status</button>
+                        </div>
+                    </div>
                     <h4 className="font-semibold text-base mt-10">Patient's Full Name</h4>
                     <input type="text" className="w-full bg-gray-200 p-2 rounded-lg h-10 outline-none font-semibold" placeholder="Full name"/>
                    <div className="flex flex-col gap-2 mt-3">
@@ -18,10 +25,10 @@ export default function Schedule() {
                     <p className="text-gray-500 font-semibold w-full text-center mt-3">Already registered? <span className="text-blue-400 font-semibold">Check your status</span></p>
                 </div> 
 
-                <div className="w-full flex justify-center items-center">
-                    <div className="relative flex justify-center items-center w-full">
-                        <img src="/injection.jpg" alt="Injection vaccine" className="w-2/5 h-80 rounded-md"/>
-                        <img src="/coronavirus.png" alt="coronavirus" className="absolute -top-6 left-34 w-20 h-16"/>
+                <div className="w-full lg:flex justify-center items-center hidden">
+                    <div className="relative flex justify-center items-center w-2/5">
+                        <img src="/injection.jpg" alt="Injection vaccine" className="w-full h-80 rounded-md"/>
+                        <img src="/coronavirus.png" alt="coronavirus" className="absolute -top-6 -left-9 w-20 h-16"/>
                         <button className="absolute bottom-5 bg-blue-700/50 shadow-lg backdrop-blur-sm py-1 px-4 rounded-lg font-semibold text-white">Verify status</button>
                     </div>
                 </div>
